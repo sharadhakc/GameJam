730bdd37b2a25ba9fd9cb1d7b7750a9a72a74824
sharadhakc <164112117+sharadhakc@users.noreply.github.com>
2025-10-31 22:28:56 +0000

    made screen

--- main.py
+++ main.py
@@ -1,1 +1,26 @@
-print("GAMEJAM")
+import pygame
+
+pygame.init() #start up pygame
+
+screenWidth=800
+screenHeight=500
+
+screen= pygame.display.set_mode((screenWidth,screenHeight))
+
+pygame.display.set_caption("SHOOTER")
+
+running=True
+
+while running:
+
+    for event in pygame.event.get():
+        pass
+
+    screen.fill((2,3,5))
+
+    pygame.display.flip()
+
+
+
+
+
