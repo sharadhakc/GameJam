3827fd74008b06b69197722ae3fb622d9cdb3bf6
sharadhakc <skc0327@my.msutexas.edu>
2025-10-31 20:07:22 -0500

    player made

--- main.py
+++ main.py
@@ -2,8 +2,8 @@ import pygame
 
 pygame.init() #start up pygame
 
-screenWidth=800
-screenHeight=500
+screenWidth=1200
+screenHeight=800
 
 screen= pygame.display.set_mode((screenWidth,screenHeight))
 
@@ -14,7 +14,8 @@ running=True
 while running:
 
     for event in pygame.event.get():
-        pass
+        if event.type== pygame.QUIT: # if they click on X it exits the screen
+            running=False
 
     screen.fill((2,3,5))
 
---
+++ player.py
@@ -0,0 +1,44 @@
+class Player:
+
+    def __init__(self, health, bul_sp, bul_count, defence, position ):
+        self.health=health
+        self.bul_sp= bul_sp
+        self.bul_count=bul_count
+        self.defence= defence
+        self.position= position # this is an array with index 0 as x and index 1 as y
+    
+    def take_dmg (self, income_dmg): # function to see how much our health decreases
+        damage= income_dmg - self.defence
+        if damage<= 0: # checking if the damage is -ve 
+            damage=0
+        print("Health=", self.health)
+        self.health-= damage
+        print("New health=", self.health)
+        print("Damage taken=", damage)
+
+    def heal(self, heal_amount ):
+        self.health+= heal_amount
+
+    def increase_speed( self, speed_up):
+        self.bul_sp+= speed_up
+    
+    def increase_bul(self, bull_up):
+        self.bul_count= (self.bul_count* bull_up)+ self.bul_count
+
+    def decrease_bul (self):
+        self.bul_count= self.bul_count- 2
+
+    def move_position(self,x):
+        self.position[0]+=x
+
+    
+
+    
+
+
+     
+    
+            
+    
+
+    
